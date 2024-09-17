693219c941f262afb14af1d6be4d324bf671ce96
jashley <jaemin.f.ashley.mil@mail.mil>
2024-09-17 10:30:38 -0400

    kernel-rop no protections complete exploit

--- kernel-rop/compress.sh
+++ kernel-rop/compress.sh
@@ -7,7 +7,7 @@ BDIR=${2:-"initramfs"}
 OUT=${3:-"initramfs-mod.cpio.gz"}
 CURR=`pwd`
 
-gcc $EXPLOIT -static -o $BDIR/exploit
+gcc $EXPLOIT -Wall -static -o $BDIR/exploit
 
 pushd $BDIR
 find . -print0 | cpio --null --format=newc -o 2>/dev/null | gzip -9 > $OUT
